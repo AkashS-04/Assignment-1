a76a979fb5bda23411a612a79dbc16b91c301b54
AkashS-04 <akash.sree004@gmail.com>
2024-12-18 15:36:58 +0530

    Updated script.js

--- public/script.js
+++ public/script.js
@@ -11,13 +11,3 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     console.log('Login Attempt:', username, password);
     // Here you would typically handle the form submission to the server
 });
-
-/*
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    console.log('Login Attempt:', username, password);
-    // Here you would typically send the data to a server
-});
-*/
